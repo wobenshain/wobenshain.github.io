8efbd014852589191e8bf1c9c4ed2be22935e39a
Wesley Obenshain <umdshaman@gmail.com>
2017-11-26 00:26:14 -0500

    initial

--- l5r-chargen/javascript/models.js
+++ l5r-chargen/javascript/models.js
@@ -1,53 +1,72 @@
 app.TemplatesModel = Backbone.Model.extend({
-    defaults: {},
-    fetch: function() {
-        var $that = this,
-            templateList = ["main","header","basics"],
-            templateObject = {},
-            deferred = $.Deferred(),
-            after = _.after(templateList.length,function() {
-                $that.set(templateObject);
-                deferred.resolve();
-            });
-        _.each(templateList,function(templateName) {
-            $.get('templates/'+templateName+'.html').done(function(response) {
-                templateObject[templateName] = response;
-                after();
-            });
-        });
-        return deferred;
-    }
+  defaults: {},
+  fetch: function() {
+    var $that = this,
+      templateList = ["main","main.header","main.core","main.traits"],
+      templateObject = {},
+      deferred = $.Deferred(),
+      after = _.after(templateList.length,function() {
+        $that.set(templateObject);
+        deferred.resolve();
+      });
+    _.each(templateList,function(templateName) {
+      $.get('templates/'+templateName+'.html').done(function(response) {
+        templateObject[templateName] = response;
+        after();
+      });
+    });
+    return deferred;
+  }
 });
 
 app.MainModel = Backbone.Model.extend({
-    defaults: {
-        header: {}
-    }
+  defaults: {
+    header: {},
+    core: {}
+  }
 });
 
 app.HeaderModel = Backbone.Model.extend({
-    defaults: {
-        xp_total: 0,
-        xp_max: 40,
-        insight: 100
-    }
-})
+  defaults: {
+    xp_total: 0,
+    xp_display: 75,
+    insight: 100
+  }
+});
 
-app.BasicsModel = Backbone.Model.extend({
-    defaults: {
-        clan: "None",
-        family: "",
-        name: "",
-        school: "",
-        pips: {
-            "Honor": 1.0,
-            "Glory": 1.0,
-            "Status": 1.0,
-            "Shadowlands Taint": 0.0,
-            "Infamy": 0.0
-        },
-        clans: {},
-        families: undefined,
-        schools: undefined
-    }
-})
+app.CoreModel = Backbone.Model.extend({
+  defaults: {
+    clan: "None",
+    family: "",
+    name: "",
+    school: "",
+    pips: {
+      "Honor": 1.0,
+      "Glory": 1.0,
+      "Status": 1.0,
+      "Shadowlands Taint": 0.0,
+      "Infamy": 0.0
+    },
+    clans: {},
+    families: undefined,
+    schools: undefined
+  }
+});
+
+app.TraitsModel = Backbone.Model.extend({
+  defaults: {
+    "air": 2,
+    "agility": 2,
+    "awareness": 2,
+    "earth": 2,
+    "fire": 2,
+    "intelligence": 2,
+    "perception": 2,
+    "reflexes": 2,
+    "stamina": 2,
+    "strength": 2,
+    "water": 2,
+    "willpower": 2,
+    "void_trait": 2
+  }
+});
--- l5r-chargen/javascript/views.js
+++ l5r-chargen/javascript/views.js
@@ -1,63 +1,84 @@
 app.MainView = Backbone.View.extend({
-    el: 'body',
-    initialize: function() {
-        this.template = _.template(app.templates.get('main'));
-        this.render.apply(this);
-        app.models.header = new app.HeaderModel();
-        app.views.header = new app.HeaderView({
-            model: app.models.header
-        });
-        app.models.basics = new app.BasicsModel({
-            clans: app.rules.clans
-        });
-        app.views.basics = new app.BasicsView({
-            model: app.models.basics
-        });
-    },
-    render: function() {
-        this.$el.html(this.template(
-            _.extend(
-                {},
-                this.model.attributes,
-                { 'ruleset': app.rules.ruleset }
-            )
-        ));
-    }
+  el: 'body',
+  initialize: function() {
+    this.template = _.template(app.templates.get('main'));
+    this.render.apply(this);
+    app.models.header = new app.HeaderModel();
+    app.views.header = new app.HeaderView({
+      model: app.models.header
+    });
+    app.models.core = new app.CoreModel({
+      clans: app.rules.clans
+    });
+    app.views.core = new app.CoreView({
+      model: app.models.core
+    });
+    app.models.traits = new app.TraitsModel();
+    app.views.traits = new app.TraitsView({
+      model: app.models.traits
+    });
+  },
+  render: function() {
+    this.$el.html(this.template(
+      _.extend(
+        {},
+        this.model.attributes,
+        { 'ruleset': app.rules.ruleset }
+      )
+    ));
+  }
 });
 
 app.HeaderView = Backbone.View.extend({
-    el: '#header',
-    initialize: function() {
-        this.template = _.template(app.templates.get('header'));
-        this.render.apply(this);
-    },
-    render: function() {
-        this.$el.html(this.template(this.model.attributes));
-    }
+  el: '#header',
+  initialize: function() {
+    this.template = _.template(app.templates.get('main.header'));
+    this.render.apply(this);
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+  }
 });
 
-app.BasicsView = Backbone.View.extend({
-    el: '#tab-basics',
+app.CoreView = Backbone.View.extend({
+  el: '#tab-core',
+  initialize: function() {
+    this.template = _.template(app.templates.get('main.core'));
+    this.model.on({
+      'change:families': this.render
+    },this);
+    this.render.apply(this);
+  },
+  events: {
+    'change [name="clan"]': function(e) {
+      var val = $(e.target).val(),
+          clan = this.model.get('clans')[val],
+          schools = {};
+      for (var school in clan.schools) {
+        schools[school] = _.extend({},clan.schools[school],app.rules.schools[school]);
+      }
+      this.model.set({
+        'clan': val,
+        'family': "",
+        'families': clan.families,
+        'schools': schools
+      });
+    }
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+  }
+});
+
+app.TraitsView = Backbone.View.extend({
+  el: '#tab-traits',
     initialize: function() {
-        this.template = _.template(app.templates.get('basics'));
-        this.model.on({
-            'change:families': this.render
-        },this);
-        this.render.apply(this);
+      this.template = _.template(app.templates.get('main.traits'));
+      this.render.apply(this);
     },
     events: {
-        'change [name="clan"]': function(e) {
-            var val = $(e.target).val(),
-                clan = this.model.get('clans')[val];
-            this.model.set({
-                'clan': val,
-                'family': "",
-                'families': clan.families,
-                'schools': clan.schools
-            });
-        }
     },
     render: function() {
-        this.$el.html(this.template(this.model.attributes));
+      this.$el.html(this.template(this.model.attributes));
     }
 });
