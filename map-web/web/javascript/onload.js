3ac5af01a545e46779d1b37535cf984b542f03f4
Wesley Obenshain <umdshaman@gmail.com>
2018-04-24 13:39:33 -0400

    nope

--- map-web/web/javascript/onload.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    app.templates = new app.TemplatesModel();
-    app.templates.fetch().done(function() {
-        app.models.main = new app.MainModel();
-        app.views.main = new app.MainView({
-            model: app.models.main
-        });
-    });
-});
